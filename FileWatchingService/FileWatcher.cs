a2975a00a1f51cad2192b1a6445d3ec8154e3866
Tuna Çandır <58295104+tunacandir@users.noreply.github.com>
2020-08-05 15:58:17 +0300

    Add project files.

---
+++ FileWatchingService/FileWatcher.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.IO; // Filesystemwatcher bunun içerisinde bulunuyor.
+
+namespace FileWatchingService
+{
+    public class FileWatcher
+    {
+        private readonly FileSystemWatcher _fileWatcher; //_fileWatcher isminde bir dosya okuyucu belirliyoruz
+
+        public FileWatcher()
+        {
+            _fileWatcher = new FileSystemWatcher(PathLocation()); 
+
+            _fileWatcher.Created += _fileWatcher_Created;
+            _fileWatcher.Renamed += _fileWatcher_Renamed;
+            _fileWatcher.Deleted += _fileWatcher_Deleted;
+            _fileWatcher.Changed += _fileWatcher_Changed;
+            _fileWatcher.IncludeSubdirectories = true;
+            _fileWatcher.EnableRaisingEvents = true;
+        }
+
+        private string PathLocation()
+        {
+            var value = "";
+            try
+            {
+                //value = ConfigurationManager.AppSettings.Get("location");
+                value = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return value;
+        }
+
+       
+
+        private void _fileWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(string.Format("File Changed: Path:{0}, Name:{1} \n", e.FullPath, e.Name));
+        }
+
+        private void _fileWatcher_Renamed(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(string.Format("File Renamed: Path:{0}, Name:{1}", e.FullPath, e.Name));
+        }
+
+        private void _fileWatcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(string.Format("File Deleted: Path:{0}, Name:{1}", e.FullPath, e.Name));
+        }
+
+        private void _fileWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(string.Format("File Created: Path:{0}, Name:{1}", e.FullPath, e.Name));
+        }
+    }
+}
---
+++ FileWatchingService/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FileWatchingService
+{
+    public static class Logger
+    {
+        public static void Log(string message)
+        {
+            try
+            {
+                string _message = String.Format("{0} {1}", message, Environment.NewLine);
+                File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "logFile.log", _message);
+            }
+            catch (Exception ex)
+            {
+                //Implement logging on next version
+            }
+        }
+    }
+}
